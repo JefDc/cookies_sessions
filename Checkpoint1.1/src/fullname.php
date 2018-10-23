dda3bcfd84387e19d231902e344c369df1a59fee
Jef De CONTI <jefdc05@gmail.com>
2018-10-23 17:22:55 +0200

    initialize project

---
+++ Checkpoint1.1/src/fullname.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jefdc
+ * Date: 14/10/2018
+ * Time: 17:46
+ */
---
+++ cookies_sessions/logOut.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jefdc
+ * Date: 15/10/2018
+ * Time: 16:50
+ */
